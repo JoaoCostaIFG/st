5d5b5d2ebc92eb4acfb70661bb7d5df65480c4a5
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-01-29 14:41:27 +0000

    Revert "Font"
    This reverts commit 4ec3e20b9de1673a485200b8261fe9d8dbf8ac70.

--- config.h
+++ config.h
@@ -5,8 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char* font   = "mono:pixelsize=12:antialias=true:autohint=true";
-static char* font   = "mono:pixelsize=12";
+static char* font   = "mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
